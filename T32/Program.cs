7523989711303332a94b273a7f2e4dabb5f17476
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-09 14:35:17 +0300

    Add T32

---
+++ T32/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу замены элементов массива: положительные элементы
+// массива замените на ссответствующие отрицательные, и наоборот
+
+// =======================
+int[] CreateRndArray(int size, int min, int max)
+{
+    int[] rndArray = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rndArray.Length; i ++)
+    {
+        rndArray[i] = rnd.Next(min, max + 1);
+    }
+    return rndArray;
+}
+// =======================
+void PrintArray(int[] array, string sep = " ")
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}{sep} ");
+        else if (i < array.Length - 1) Console.Write($"{array[i]}{sep} ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+int[] NegArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+// =======================
+Console.Write("Введите размерность массива :");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите нижнюю границу чисел массива :");
+int arrMinNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите верхнюю границу чисел массива :");
+int arrMaxNum = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateRndArray(arrSize, arrMinNum, arrMaxNum);
+PrintArray(arr, ",");
+
+Console.WriteLine();
+int[]negarr = NegArray(arr);
+PrintArray(negarr, ",");
